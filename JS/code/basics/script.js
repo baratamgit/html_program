26cdd0ca8147f7c28d8883eaf76e0a345298b8b9
baratamgit <shanmukhasrinivasbaratam630@gmail.com>
2024-12-18 17:20:11 +0530

    Add files via upload

---
+++ JS/code/basics/script.js
@@ -0,0 +1,24 @@
+// write js code to add two numbers
+// let a = 10, b = 20;
+// let c = a + b;
+// console.log("The addition is " + c);
+// /* write js code to substract two numbers */
+// let v = 10, h = 20;
+// let f = v - h;
+// alert("The subtraction is " + f);
+// console.log("shannu");
+
+//write js code to multiply two numbers and alert the results. take numbers from prompt
+/*let p = -prompt("enter p value");
+let g = +prompt("enter g value");
+let w = p * g;
+confirm("it is multiplying two numbers, is it okay?")
+alert("The multiply is " + w);
+console.log("dfd");*/
+/* take user input of length and breadth and find area of rectangle and alert the result */
+let lenght = +prompt("enter p value");
+let breadth = +prompt("enter g value");
+let area = lenght * breadth;
+confirm("it is multiplying two numbers, is it okay?")
+alert("Area of rectangle is " + area);
+console.log("dfd");
